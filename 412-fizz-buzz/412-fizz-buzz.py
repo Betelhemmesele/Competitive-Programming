761c1ef6e4bb76f037e7d9c81b7c70f2c4e099c0
Betelhemmesele <88847580+Betelhemmesele@users.noreply.github.com>
2023-04-20 03:50:26 -0700

    Update 412-fizz-buzz.py

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,20 +1,14 @@
-class Solution(object):
-   def fizzBuzz(self, n):
-      """
-      :type n: int
-      :rtype: List[str]
-      """
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
       result = []
       for i in range(1,n+1):
-         if i% 3== 0 and i%5==0:
-            result.append("FizzBuzz")
-         elif i %3==0:
-            result.append("Fizz")
-         elif i% 5 == 0:
-            result.append("Buzz")
-         else:
-            result.append(str(i))
+          if i%3==0 and i%5==0:
+              result.append('FizzBuzz')
+          elif i%3==0:
+              result.append('Fizz')
+          elif i%5==0:
+              result.append('Buzz')
+          else:
+              result.append(str(i))
       return result
-ob1 = Solution()
-print(ob1.fizzBuzz(30))
         
